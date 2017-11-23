b23d9f4e54f66b5c775b5efb4849b3df55587a40
준형 배 <bjh6681@outlook.kr>
2017-11-23 23:36:21 +0900

    싱글톤 추가

---
+++ 20171121_FindKoreaHistory/20171121_FindKoreaHistory/MainProc.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "stdafx.h"
+
+class MainProc
+{
+private:
+	SDL_Window* pWindow;
+
+};
---
+++ 20171121_FindKoreaHistory/20171121_FindKoreaHistory/Singletone.h
@@ -0,0 +1,32 @@
+#pragma once
+
+template <typename class T>
+class Singletone
+{
+protected:
+	Singletone() {};
+	virtual ~Singletone() {};
+
+public:
+	inline static T* GetInstance()
+	{
+		if (m_pInstance == NULL)
+		{
+			m_pInstance = new T;
+		}
+		return m_pInstance;
+	}
+	inline static void ReleaseInstance()
+	{
+		if (m_pInstance)
+		{
+			delete m_pInstance;
+			m_pInstance = NULL;
+		}
+	}
+
+private:
+	static T* m_pInstance;
+};
+
+template <typename T> T* Singletone<T>::m_pInstance = NULL;
